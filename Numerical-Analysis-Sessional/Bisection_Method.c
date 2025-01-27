c8d86b3b29bca34e704c4be84d16187fc96e341c
Md. Afif Afroz Fahim <mdafifafrozfahim@gmail.com>
2025-01-27 20:30:32 +0600

    create

---
+++ Numerical-Analysis-Sessional/Bisection_Method.c
@@ -0,0 +1,74 @@
+#include<stdio.h>
+#include<math.h>
+
+double f(double x)
+{
+    return x*x-7;
+}
+
+
+void bisection_method(double a,double b)
+{
+    int i=0;
+    double c=0.0,c_old=0.0,tol=0.05;
+    double z=0;
+
+    if(f(a)*f(b)>=0)
+    {
+        printf("Invalid interval f(a) and f(b) must have opposite signs ");
+        return;
+    }
+     printf("\nIteration\t a\t\t b\t\t c\t\t F(a)\t\t F(b)\t\tF(c)\t\t  error \n");
+     printf("------------------------------------------------------------------------------------------------------------------------");
+
+     do
+     {
+         c_old=c;
+         c=(a+b)/2.0;
+         double fc=f(c);
+         double fa=f(a);
+         double fb=f(b);
+
+         if (c != 0)
+         {
+             z = fabs((c - c_old) / c);
+         }
+         else
+         {
+             z = fabs(c - c_old);
+         }
+
+         printf("%d\t%lf\t%lf\t%lf\t%lf\t%lf\t%lf\t%lf\n",i,a,b,c,fa,fb,fc,z);
+
+         if(fc==0.0)
+         {
+             break;
+         }
+         else if(fa*fc<0)
+         {
+             b=c;
+         }
+         else
+         {
+             a=c;
+
+         }
+
+
+         i++;
+     }
+     while(z>=tol);
+     printf("\n ROOT is =%.4lf\n",c);
+}
+
+
+
+int main()
+{
+    double a,b;
+    printf("Enter interval [a, b]: ");
+    scanf("%lf %lf",&a,&b);
+    bisection_method(a,b);
+
+    return 0;
+}
