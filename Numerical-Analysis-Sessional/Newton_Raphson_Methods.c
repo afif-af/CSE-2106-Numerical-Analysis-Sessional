5229f66bfcfea8094341b9fbee4d816efcfe2ad7
Md. Afif Afroz Fahim <mdafifafrozfahim@gmail.com>
2025-01-27 21:06:33 +0600

    create

---
+++ Numerical-Analysis-Sessional/Newton_Raphson_Methods.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <math.h>
+
+double function(double x)
+{
+    return pow(x, 3) - 2 * x - 5;
+}
+double functionDerivative(double x)
+{
+    return 3 * pow(x, 2) - 2;
+}
+void newton_Raphson_methods(double x0, double tol)
+{
+    double x = x0;
+    double fx, dfx, er;
+    int i = 0;
+    const int maxIterations = 100;
+    printf("Iteration\t x\t\t f(x)\t\t f'(x)\t\t Error\n");
+    printf("--------------------------------------------------------------------\n");
+
+    do
+    {
+        fx = function(x);
+        dfx = functionDerivative(x);
+        if (dfx == 0.0)
+        {
+            printf("Derivative is zero. Cannot continue.\n");
+            return;
+        }
+
+        double x_new = x - fx / dfx;
+        er = fabs(x_new - x);
+        printf("%d\t\t%lf\t%lf\t%lf\t%lf\n", i, x, fx, dfx, er);
+        x = x_new;
+        i++;
+
+        if (i >= maxIterations)
+        {
+            printf("..\n");
+            return;
+        }
+
+    } while (er > tol);
+
+    printf("\nRoot is = %lf\n", x);
+}
+
+int main()
+{
+    double x0, tol;
+    printf("Enter initial guess (x0): ");
+    scanf("%lf", &x0);
+    printf("Enter tolerance: ");
+    scanf("%lf", &tol);
+    newton_Raphson_methods(x0,tol);
+
+    return 0;
+}
